68643db5252fc92ffac6c95527cf2c28e73037ab
ravisenevirathne <ravi2005@gmail.com>
2023-08-30 20:51:57 +1000

    eks added

--- eks_terraform/vpc.tf
+++ eks_terraform/vpc.tf
@@ -13,6 +13,8 @@ module "myapp-vpc" {
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets = var.public_subnet_cidr_blocks
   azs = data.aws_availability_zones.azs.names
+  access_key = var.access_key
+  secret_key = var.secret_key
 
   enable_nat_gateway = true
   single_nat_gateway = true
